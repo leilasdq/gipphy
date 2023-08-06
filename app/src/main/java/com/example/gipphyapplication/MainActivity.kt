74fd28b4faec8327b3781214c8b3b07aedbbcb53
leila_sdq <leila.sedighi.2020@gmail.com>
2023-08-06 23:04:23 +0330

    feat: create custom application class to startKoin

--- app/src/main/java/com/example/gipphyapplication/MainActivity.kt
+++ app/src/main/java/com/example/gipphyapplication/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+
+    private val hello by inject<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Finallyyyyy")
+            Text(hello)
         }
     }
 }
